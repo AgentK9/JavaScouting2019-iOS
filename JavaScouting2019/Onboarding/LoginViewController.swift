3c06be6bdae2631c26ec134a38372a1c4692a89c
K Bee <dogsragents2@gmail.com>
2019-02-12 19:25:17 -0500

    Login UI in place. No segues there yet.

---
+++ JavaScouting2019/Onboarding/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  JavaScouting2019
+//
+//  Created by Keegan on 2/12/19.
+//  Copyright © 2019 JavaScouts. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+	@IBOutlet var emailTextField: UITextField!
+	@IBOutlet var userNameTextField: UITextField!
+	@IBOutlet var displayNameTextField: UITextField!
+	@IBOutlet var pass1TextField: UITextField!
+	@IBOutlet var pass2TextField: UITextField!
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
