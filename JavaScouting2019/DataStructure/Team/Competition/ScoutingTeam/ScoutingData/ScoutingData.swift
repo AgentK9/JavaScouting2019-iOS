839f7b1d85cf1703010ccc1ac9a5b770362b72e5
K Bee <dogsragents2@gmail.com>
2019-02-16 00:21:52 -0500

    Data Frameworks in

--- JavaScouting2019/DataStructure/Team/Competition/ScoutingTeam/ScoutingData/ScoutingData.swift
+++ JavaScouting2019/DataStructure/Team/Competition/ScoutingTeam/ScoutingData/ScoutingData.swift
@@ -13,8 +13,72 @@ struct ScoutingData: Codable {
 	var land: Bool
 	var sample: Int
 	var claim: Bool
-	var park: Int
+	var park: Bool
 	var depotMin: Int
 	var landerMin: Int
 	var endGame: Int
+	
+	func autoPts() -> Int {
+		var total: Int = 0
+		
+		switch land {
+		case true:
+			total += 30
+		default:
+			total += 0
+		}
+		switch sample {
+		case 0:
+			total += 50
+		case 1:
+			total += 25
+		case 3:
+			total += 8
+		default:
+			total += 0
+		}
+		switch claim {
+		case true:
+			total += 15
+		default:
+			total += 0
+		}
+		switch park {
+		case true:
+			total += 10
+		default:
+			total += 0
+		}
+		
+		return total
+	}
+	func teleOpPts() -> Int {
+		var total: Int = 0
+		
+		total += depotMin * 2
+		total += landerMin * 5
+		
+		return total
+	}
+	func endGamePts() -> Int {
+		var total: Int = 0
+		switch endGame {
+		case 0:
+			total += 50
+		case 1:
+			total += 25
+		case 2:
+			total += 15
+		default:
+			total += 0
+		}
+		return total
+	}
+	func totalPts() -> Int {
+		var total: Int = 0
+		total += autoPts()
+		total += teleOpPts()
+		total += endGamePts()
+		return total
+	}
 }
--- JavaScouting2019/DataStructure/Team/Competition/ScoutingTeam/ScoutingTeam.swift
+++ JavaScouting2019/DataStructure/Team/Competition/ScoutingTeam/ScoutingTeam.swift
@@ -16,4 +16,64 @@ struct ScoutingTeam: Codable {
 	var latestScore: Int?
 	var path: String
 	var scouting: [ScoutingData]?
+	
+	private func getListOf(type: String) -> [Int] {
+		var list: [Int] = [Int]()
+		
+		if scouting != nil {
+			for item in scouting! {
+				switch type {
+				case "auto":
+					list.append(item.autoPts())
+				case "tele":
+					list.append(item.teleOpPts())
+				case "end":
+					list.append(item.endGamePts())
+				case "total":
+					list.append(item.totalPts())
+				default:
+					print("unknown type")
+				}
+			}
+		}
+		
+		return list
+	}
+	
+	func avgScore(type: String) -> Float {
+		let list: [Int] = getListOf(type: type)
+		var total: Int = 0
+		
+		for item in list {
+			total += item
+		}
+		
+		let average: Float = Float(total)/Float(list.count)
+		return average
+		
+	}
+	func highScore(type: String) -> Float {
+		let list: [Int] = getListOf(type: type)
+		var maximum = 0
+		
+		for item in list {
+			if item > maximum {
+				maximum = item
+			}
+		}
+		
+		return Float(maximum)
+	}
+	func lowScore(type: String) -> Float {
+		let list: [Int] = getListOf(type: type)
+		var minimum = 1500
+		
+		for item in list {
+			if item < minimum {
+				minimum = item
+			}
+		}
+		
+		return Float(minimum)
+	}
 }
