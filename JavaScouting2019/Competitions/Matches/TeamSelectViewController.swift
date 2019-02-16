1d964cbb3c527089c89d1afa8fcdee68c4c4fa69
K Bee <dogsragents2@gmail.com>
2019-02-16 16:06:04 -0500

    Matches Work!

---
+++ JavaScouting2019/Competitions/Matches/TeamSelectViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TeamSelectViewController.swift
+//  JavaScouting2019
+//
+//  Created by Keegan on 2/16/19.
+//  Copyright © 2019 JavaScouts. All rights reserved.
+//
+
+import UIKit
+
+class TeamSelectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ JavaScouting2019/Data/Path.swift
@@ -0,0 +1,9 @@
+//
+//  Path.swift
+//  JavaScouting2019
+//
+//  Created by Keegan on 2/16/19.
+//  Copyright © 2019 JavaScouts. All rights reserved.
+//
+
+import Foundation
